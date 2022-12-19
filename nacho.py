396bf72bd0fa42ba5ba091396953a545a37a6248
Donovan Adrian <websurfer21020@gmail.com>
2022-12-18 22:00:15 -0700

    Updated nacho.py File
    Updated nacho Python file with progress on check_drives, pre_screen_drive, select_drive, and main functions. Also added prep_destination_drive for directory initialization, if needed.

--- nacho.py
+++ nacho.py
@@ -20,26 +20,48 @@ def welcome_message():
 
 
 def check_drives():
+    global connectedDrives
     drives = [chr(x) + ':' for x in range(65, 91) if os.path.exists(chr(x) + ':')]
     print(drives)
     print(type(drives))
-    # Loop through connected drives (print out #-n) and ask user which drive to select for A, source, and B, destination
-    # When checking source drive, pre-screen drive and look for valid files
-    print('Save the above drives to connectedDrives')
-    # If there are no drives found... Exit? I guess?
+    drive_num = 0
+    for drive in drives:
+        print(str(drive_num) + ' -> ' + drive)
+        drive_num += drive_num
+        connectedDrives.append(drive)
+
+    if drive_num <= 1:
+        clean_exit('Not enough drives are connected! Please connect at least two drives to the system!')
 
 
 def pre_screen_drive():
-    print('This will check the source drive, ' + sourceDrive)
+    global connectedDrives
+    global sourceDrive
+    print('Checking Source Drive For Valid Files...')
+    check_user_dir = os.path.isdir(connectedDrives[sourceDrive] + '\\Users')
+    if not check_user_dir:
+        clean_exit('The source drive did not contain the expected user files. Please try again! Exiting...')
+
+
+def prep_destination_drive():
+    global connectedDrives
+    global destinationDrive
+    check_destination_dir = os.path.isdir(connectedDrives[destinationDrive], '\\NachoPyDrop')
+    if not check_destination_dir:
+        print('Creating New Directory For Destination Drive...')
+        path = os.path.join(connectedDrives[destinationDrive], '\\NachoPyDrop')
+        os.mkdir(path)
 
 
 def select_drive(drive_string):
     global destinationDrive
     global sourceDrive
+    global connectedDrives
     print('Please select a ' + drive_string + ' drive')
     print(drive_string)
     if drive_string == "destination":
         destinationDrive = _input('', int)
+        prep_destination_drive()
     elif drive_string == "source":
         sourceDrive = _input('', int)
         pre_screen_drive()
@@ -49,13 +71,13 @@ def _input(message, input_type=str):
     while True:
         try:
             return input_type (input(message))
-        except:pass
+        except:
+            clean_exit('You entered an invalid value that was not expected! Exiting...')
 
 
 def collect_files():
     print('This doesn\'t do anything yet!')
-    # Look in the selected drive for /user/ files. Collect all files within and copy to desired drive using copy()
-    # I don't remember if shutil does directories AND files?
+    # Look in the selected drive for <source_letter>/Users/ iterate through users. Use copy() to gather files
 
 
 def copy(src, dst):
@@ -64,7 +86,7 @@ def copy(src, dst):
     except shutil.SameFileError:
         print('Error: The source and destination are the same!')
     except PermissionError:
-        print('Error: Permission denied!')
+        print('Error: Permission denied! Try running this script as administrator!')
     except:
         print('Error: Could not copy this file!')
 
@@ -75,7 +97,8 @@ def main():
     
     print('Checking Connected Drives...')
     check_drives()
-    # print('Checking For Valid Files...')
+    select_drive('source')
+    select_drive('destination')
     # collect_files()
     clean_exit('Script complete!')
 
@@ -86,11 +109,6 @@ main()
 
 '''
 Other stuff
-out = os.path.isdir('C:\\Users')
-out = os.path.isfile('C:\\Users\foo.csv')
-cwd = os.getcwd()
-path = os.path.join(parent_dir, directory)
-os.mkdir(path)
 for file in os.listdir(rootdir):
     d = os.path.join(rootdir, file)
     if os.path.isdir(d):
@@ -98,7 +116,4 @@ for file in os.listdir(rootdir):
 my_list = ['blah', 'foo', 'bar']
 if item in my_list:
     # whatever
-
-Just in case
-https://www.geeksforgeeks.org/implementation-of-dynamic-array-in-python/
 '''
