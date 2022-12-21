d44056645a27cef24f2a7821054f2bcda1405ef8
Donovan Adrian <websurfer21020@gmail.com>
2022-12-20 22:27:34 -0700

    Updated nacho.py File
    Updated nacho Python file with drive_num iteration correction, check_destination_dir check correction, output formatting updates, fleshed out collect_files function, and added main calls.

--- nacho.py
+++ nacho.py
@@ -16,49 +16,47 @@ def clean_exit(message):
 
 
 def welcome_message():
-    print('\n******************************\nWelcome to the NachoPy Script!\n******************************\n')
+    print('\n\n******************************\nWelcome to the NachoPy Script!\n******************************\n')
 
 
 def check_drives():
     global connectedDrives
     drives = [chr(x) + ':' for x in range(65, 91) if os.path.exists(chr(x) + ':')]
     print(drives)
-    print(type(drives))
     drive_num = 0
     for drive in drives:
         print(str(drive_num) + ' -> ' + drive)
-        drive_num += drive_num
+        drive_num += 1
         connectedDrives.append(drive)
 
     if drive_num <= 1:
-        clean_exit('Not enough drives are connected! Please connect at least two drives to the system!')
+        clean_exit('\nError: Not enough drives are connected! Please connect at least two drives to the system!')
 
 
 def pre_screen_drive():
     global connectedDrives
     global sourceDrive
-    print('Checking Source Drive For Valid Files...')
+    print('\n\nChecking Source Drive For Valid Files...')
     check_user_dir = os.path.isdir(connectedDrives[sourceDrive] + '\\Users')
     if not check_user_dir:
-        clean_exit('The source drive did not contain the expected user files. Please try again! Exiting...')
+        clean_exit('\nError: The source drive did not contain the expected user files. Please try again! Exiting...')
 
 
 def prep_destination_drive():
     global connectedDrives
     global destinationDrive
-    check_destination_dir = os.path.isdir(connectedDrives[destinationDrive], '\\NachoPyDrop')
+    check_destination_dir = os.path.isdir(connectedDrives[destinationDrive] + '\\NachoPyDrop')
     if not check_destination_dir:
-        print('Creating New Directory For Destination Drive...')
+        print('\n\nCreating New Directory For Destination Drive...')
         path = os.path.join(connectedDrives[destinationDrive], '\\NachoPyDrop')
         os.mkdir(path)
 
 
 def select_drive(drive_string):
+    global connectedDrives
     global destinationDrive
     global sourceDrive
-    global connectedDrives
-    print('Please select a ' + drive_string + ' drive')
-    print(drive_string)
+    print('\n\nPlease select a ' + drive_string + ' drive from 0 to ' + str(len(connectedDrives)))
     if drive_string == "destination":
         destinationDrive = _input('', int)
         prep_destination_drive()
@@ -72,35 +70,49 @@ def _input(message, input_type=str):
         try:
             return input_type (input(message))
         except:
-            clean_exit('You entered an invalid value that was not expected! Exiting...')
+            print('Please only enter a ' + str(input_type) + '! Try again...\n')
+            pass
 
 
 def collect_files():
-    print('This doesn\'t do anything yet!')
-    # Look in the selected drive for <source_letter>/Users/ iterate through users. Use copy() to gather files
+    global connectedDrives
+    global destinationDrive
+    global sourceDrive
+    source_dir = os.path.join(connectedDrives[sourceDrive], '\\Users')
+    dest_dir = os.path.join(connectedDrives[destinationDrive], '\\NachoPyDrop')
+    for file in os.listdir(source_dir):
+        copy(file, dest_dir)
 
 
 def copy(src, dst):
+    print('\nCopying ' + src + '...')
     try:
         shutil.copy(src, dst)
     except shutil.SameFileError:
-        print('Error: The source and destination are the same!')
+        print('\nError: The source and destination are the same!')
     except PermissionError:
-        print('Error: Permission denied! Try running this script as administrator!')
+        print('\nError: Permission denied! Try running this script as administrator!')
     except:
-        print('Error: Could not copy this file!')
+        print('\nError: Could not copy this file!')
 
 
 def main():
+    global connectedDrives
+    global destinationDrive
+    global sourceDrive
     if platform.system() != 'Windows':
         clean_exit('Please only use this script in Windows!')
-    
     print('Checking Connected Drives...')
     check_drives()
     select_drive('source')
-    select_drive('destination')
-    # collect_files()
-    clean_exit('Script complete!')
+    if len(connectedDrives) == 2:
+        destinationDrive = 1 - sourceDrive
+        prep_destination_drive()
+    else:
+        select_drive('destination')
+    print('\n\nCollecting Files... Please Wait...')
+    collect_files()
+    clean_exit('\nScript complete!')
 
 
 welcome_message()
